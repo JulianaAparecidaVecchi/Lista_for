4d5340098cd2e45544d51384848ba8718846a21a
JulianaAparecidaVecchi <163355132+JulianaAparecidaVecchi@users.noreply.github.com>
2024-04-12 12:02:39 -0300

    Recriação do ex10
    Recriação do ex10

--- ex03.py
+++ ex03.py
@@ -1,10 +1,17 @@
 print("--------------| TABUADA |----------------")
-tab=int(input("Digite um valor inteiro de 1 a 10: "))
-
-if tab<=10 and tab>0:
-    print(f"TABUADA DO {tab}")
-    for numtab in range(0,11):
-        mul= tab*numtab
-        print(f"{tab} X {numtab} = {mul}")
-else:
-    print("O Programa só aceita números de 1 a 10.")    
+print("===| Quando desejar encerrar a calculadora de tabuadas digite 0 |===")
+print(60*"-")
+while True:
+    tab=int(input("Digite um valor inteiro de 1 a 10: "))
+    if tab==0:
+        print("Finalizando.....")
+        break
+    
+    if tab<=10 and tab>0:
+        print(f"TABUADA DO {tab}")
+        for numtab in range(0,11):
+            mul= tab*numtab
+            print(f"{tab} X {numtab} = {mul}")
+    else:
+        print("O Programa só aceita números de 1 a 10.")  
+        continue  
--- ex10.py
+++ ex10.py
@@ -1,18 +1,8 @@
-
-lista = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6]
-
-numero_mais_repetido = None
-contagem_maxima = 0
-
-for numero in lista:
-    contagem = 0
-    
-    for outro_numero in lista:
-        if outro_numero == numero:
-            contagem += 1
-    
-    if contagem > contagem_maxima:
-        numero_mais_repetido = numero
-        contagem_maxima = contagem
-
-print("O número que se repete mais vezes é:", numero_mais_repetido)
+lista=[2,3,2,5,4,3,7,2,9,4,1,4]
+num_rep=0
+num_freq=0
+for num in lista:
+   if lista.count(num)>num_freq:
+      num_freq+=1
+      num_rep=num
+print(f"O Número repetido é {num_rep} pois apareceu {num_freq}")    
